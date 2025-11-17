eb1ed6dd6aa788beef4e789dd37b53e6e6c94292
Matheusevaristo-coder <math_eva1502@hotmail.com>
2025-11-17 17:36:22 -0300

    Primeiro commit do projeto Evaristo Solutions

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Espera o conteúdo da página carregar
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // Inicia a biblioteca de Animação (AOS)
+    AOS.init({
+        duration: 800,  // Duração da animação em milissegundos
+        once: true      // Anima apenas uma vez (quando o usuário rola para baixo)
+    });
+
+});
